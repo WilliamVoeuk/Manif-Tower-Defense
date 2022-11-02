76a2caa5bb2ae4d180f33ef4c960ac9f235405bc
VWilliam <voeuk.william@gmail.com>
2022-11-02 15:35:15 +0100

    Better WaveSpawner

--- Assets/WaveSpawner.cs
+++ Assets/WaveSpawner.cs
@@ -1,25 +1,41 @@
 
+using System.Collections;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
     [SerializeField] GameObject _protestorPrefab;
+    
     [SerializeField] float _waveTimer = 5f;
     [SerializeField] Transform _spawner;
     [SerializeField] float _countDown = 2f;
+    private int _counter = 1;
+
+
     void Update()
     {
         if(_countDown <= 0)
         {
-            StartWave();
+            StartCoroutine(StartWave());
             _countDown = _waveTimer;
 
         }
         _countDown -= Time.deltaTime;
     }
-    void StartWave()
+
+    IEnumerator StartWave()
     {
-        Debug.Log("Oya");
+        for (int i = 0; i < _counter; i++)
+        {
+            SpawnEnemies();
+            yield return new WaitForSeconds(0.5f);
+        }
+        _counter++;
+    }
+
+    void SpawnEnemies()
+    {
         Instantiate(_protestorPrefab, _spawner.position, _spawner.rotation);
+
     }
 }
