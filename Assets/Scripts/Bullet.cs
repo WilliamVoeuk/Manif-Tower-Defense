2e9756df5ac6ea77d7a4ec8f71a3f782ce478ea6
VWilliam <voeuk.william@gmail.com>
2022-11-10 00:32:07 +0100

    Good now

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,23 +7,16 @@ public class Bullet : MonoBehaviour
 
     [SerializeField] float _speed;
     [SerializeField] float _lifetime;
-
-    [SerializeField] float _explosionRadius = 0f;
     public void Seek(Transform target)
     {
         _target = target;
     }
     void Update()
     {
-        if(_target == null )
-        {
-            Destroy(gameObject);
-            return;
-        }
 
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
-        transform.LookAt(_target);
         _lifetime -= Time.deltaTime;
+
         if (_lifetime <= 0)
         {
             GameObject.Destroy(gameObject);
@@ -32,24 +25,6 @@ public class Bullet : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        
-        if (_explosionRadius > 0)
-        {
-            Explode();
-        }
-        else
-        {
-            Damage(collision);
-        }
-        GameObject.Destroy(gameObject);
-    }
-
-    void Explode()
-    {
-
-    }
-    void Damage(Collision enemy)
-    {
-        Destroy(enemy.gameObject);
+        GameObject.Destroy(collision.gameObject);
     }
 }
