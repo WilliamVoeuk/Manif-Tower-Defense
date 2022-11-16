742b0a162c2d62b35b08b698679beb25cdfa53ed
VWilliam <voeuk.william@gmail.com>
2022-11-16 15:32:56 +0100

    Main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Play()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    public void Quit()
+    {
+        Debug.Log("Cya");
+    }
+}
